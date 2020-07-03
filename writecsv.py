de07bb5fc935067a5de344b407a543617df61163
harrystudypython <52101807+harrystudypython@users.noreply.github.com>
2020-07-03 13:25:49 +0800

    Update writecsv.py

--- writecsv.py
+++ writecsv.py
@@ -1,8 +1,12 @@
+# ! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
 import pandas as pd
 import numpy as np
 
 '''第二种写法：当值为string类型的数据，此时需要加上 index=[0] 因为pandas 的dataframe需要一个可迭代的对象'''
 data2 = {'姓名': 'fuhang', '性别': '男', '昵称': '那时的吻真香'}
+#  data2 = {'name': 'fuhang', 'sex': 'man', 'name_self': 'good_feel'}
 df2=pd.DataFrame(data2,index=[0])
 df2.to_csv('Result2.csv',index=None)
 
